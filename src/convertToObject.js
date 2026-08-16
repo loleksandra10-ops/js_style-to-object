e1b7b78bfe1fa8cd7afacc313af50828df461607
Oleksandra Liubchenko <l.oleksandra10@gmail.com>
2026-08-17 01:34:15 +0300

    Implement convertToObject

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,21 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+
+  const declarations = sourceString.split(';');
+
+  for (const declaration of declarations) {
+    if (!declaration.trim()) {
+      continue;
+    }
+
+    const [key, ...valueParts] = declaration.split(':');
+
+    result[key.trim()] = valueParts.join(':').trim();
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
